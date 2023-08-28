985eea0e61a7c795811142654ec679cde9b7a793
mwidas <mel.widas@gmail.com>
2023-08-28 22:27:12 +0000

    created dataset on desserts

---
+++ desserts_match_mw.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(rvest)  # use to scrape website content
+library(here)
+
+# Check if that data folder exists and creates it if not
+dir.create("data", showWarnings = FALSE)
+
+# Read the webpage code
+webpage <- read_html("https://www.eatthis.com/iconic-desserts-united-states/")
+
+# Extract the desserts listing
+dessert_elements<- html_elements(webpage, "h2")
+dessert_listing <- dessert_elements %>% 
+  html_text2() %>%             # extracting the text associated with this type of elements of the webpage
+  as_tibble() %>%              # make it a data frame
+  rename(dessert = value) %>%  # better name for the column
+  head(.,-3) %>%               # 3 last ones were not desserts 
+  rowid_to_column("rank") %>%  # adding a column using the row number as proxy for the rank
+  write_csv("data/iconic_desserts.csv") # save it as csv
+
+dessert_ms <- read_csv(here("data", "favorite_desserts,ms.csv")) %>% 
+  rename(dessert = Favorite_dessert)
+
+semi_join(dessert_listing, dessert_ms)
